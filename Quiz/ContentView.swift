860af66fce9dd2f0023d88a1fdcc60f0f18fecc8
Sunjae.Park <sunjae.park.kr@gmail.com>
2019-06-24 21:16:23 +0900

    Alert

--- Quiz/ContentView.swift
+++ Quiz/ContentView.swift
@@ -1,38 +1,46 @@
-//
-//  ContentView.swift
-//  Quiz
-//
-//  Created by heaven on 2019/06/17.
-//  Copyright © 2019 heaven. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView : View {
     var item: QuizItem
     @State var isCorrect: Bool = false
+    @State var isShowAlert: Bool = false
     
     var body: some View {
         VStack {
             Text(verbatim: item.question)
-                .lineLimit(nil)
                 .font(.title)
+                .lineLimit(nil)
             Spacer()
             AnswerButton(title: "true",
                          color: .blue,
-                         action: { self.isCorrect = self.correct(by: "True")} )
+                         action: {
+                            self.isCorrect = self.correct(by: "True")
+                            self.isShowAlert = true
+            })
             AnswerButton(title: "false",
                          color: .red,
-                         action: { self.isCorrect = self.correct(by: "False")} )
+                         action: {
+                            self.isCorrect = self.correct(by: "False")
+                            self.isShowAlert = true
+            })
             }
             .padding()
-            .presentation($isCorrect) {
-                Alert(title: Text("Done"))
+            .presentation($isShowAlert) {
+                Alert(title: Text(alertMessage(by: isCorrect)))
         }
     }
     
     func correct(by answer: String) -> Bool {
         return item.correctAnswer == answer
+    }
+    
+    func alertMessage(by isCorrect: Bool) -> String {
+        switch isCorrect {
+        case true:
+            return "정답!!!!!"
+        case false:
+            return "오답ㅠ"
+        }
     }
 }
 
